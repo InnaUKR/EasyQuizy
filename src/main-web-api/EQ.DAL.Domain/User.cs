83ebaf3caf1876ec90ffc303df27c0dfda7d14ea
AndreyFilonenko <an.v.filonenko@gmail.com>
2019-04-26 08:57:47 +0300

    user repo and domain model

---
+++ src/main-web-api/EQ.DAL.Domain/User.cs
@@ -0,0 +1,11 @@
+﻿using EQ.Common.Models.Enums;
+using EQ.DAL.Infrastructure.Implementation;
+using System;
+
+namespace EQ.DAL.Domain
+{
+    public class User : UserEntity<Guid, string>
+    {
+        public UserStatus Status { get; set; }
+    }
+}
